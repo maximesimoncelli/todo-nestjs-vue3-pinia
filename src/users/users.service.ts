ad7219606c851afc5d3fbb1617f6527d7fa4a6f9
Maxime Simoncelli <maxime.simoncelli@prestashop.com>
2022-04-20 17:22:25 +0200

    Init

---
+++ src/users/users.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+
+export type User = {
+  userId: number;
+  username: string;
+  password: string;
+};
+
+@Injectable()
+export class UsersService {
+  private readonly users: User[] = [
+    {
+      userId: 1,
+      username: 'maria@nest.js',
+      password: '1234',
+    },
+    {
+      userId: 2,
+      username: 'joe@nest.js',
+      password: '1234',
+    },
+  ];
+
+  async findOne(username: string): Promise<User | undefined> {
+    return this.users.find((user) => user.username === username);
+  }
+}
